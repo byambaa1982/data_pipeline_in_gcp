e394889de3b421db343157549317bff5d80d8c1c
Byambatsogt Enkhbat <enkhbat@Byambatsogts-MacBook-Pro.local>
2021-02-10 10:54:17 -0600

    make short reading time using pyexcelrate lib

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import datetime
 from io import StringIO
 from io import BytesIO
 import os
+from pyexcelerate import Workbook
+import time
 
 project_id='your-project-id'
 destination_bucket='your-dest-bucket'
@@ -13,16 +15,29 @@ def chunk_csv_file(event,destination_bucket):
 # it is mandatory initialize the storage client
     client = storage.Client()
     #please change the file's URI
-    filename=event['name']
-    myurl='gs://car_datalake/'+str(filename)
+    fname=event['name']
+    myurl='gs://car_datalake/'+str(fname)
     print('Uploaded file: {}'.format(event['name']))
-    for i,chunk in enumerate(pd.read_csv(myurl, chunksize=100000)):
+    for i,chunk in enumerate(pd.read_csv(myurl, chunksize=1000000)):
         print('chunk shape is {}'.format(chunk.shape))
-        filename=('chunk{}.xlsx'.format(i))
-        f = BytesIO()
-        chunk.to_excel(f)
-        f.seek(0)
-        bucket=client.get_bucket('result_excel_files')
-        newblob=blob(filename)
-        newblob.upload_from_file(f, content_type='xlsx')
+        print('loop is {}'.format(i))
+        print('chunk shape is {}'.format(chunk.shape))
+        try:
+            # start = datetime.datetime.now()[11:19]
+            filename=('chunk{}.xlsx'.format(i))
+            print(filename)
+            f = BytesIO()
+            # chunk.to_excel(f)
+            start = time.time()
+            wb = Workbook()
+            wb.new_sheet("car data", data=chunk)
+            wb.save(f)
+            end = time.time()
+            print("Read csv and to excel: ",(end-start),"sec")
+            f.seek(0)
+            bucket=client.get_bucket('result_excel_files')
+            newblob=bucket.blob(filename)
+            newblob.upload_from_file(f, content_type='xlsx')
+        except:
+            print('something wrong: {}'.format(i))
     return f'check the results in the logs'
