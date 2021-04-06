2d0a47a53ce41ef7dcce28cbfcd22d16bdf0148f
Byambatsogt Enkhbat <enkhbat@Byambatsogts-MacBook-Pro.local>
2021-04-06 12:12:48 -0500

    change cols

--- rename_change_csv/change_and_save.py
+++ rename_change_csv/change_and_save.py
@@ -5,22 +5,22 @@ from io import StringIO
 from io import BytesIO
 import os
 
-os.environ["GCLOUD_PROJECT"] = "twittersheet-275317"
-project_id='twittersheet-275317'
-destination_bucket='getting-termites-tweet'
+os.environ["GCLOUD_PROJECT"] = "juva-brands"
+project_id='juva-brands'
+destination_bucket='final-amazon-brand-analytics-search-terms-12345'
 
 def change_and_save_csv_file(event,destination_bucket):
 # it is mandatory initialize the storage client
     client = storage.Client()
     #please change the file's URI
     fname=event['name']
-    myurl='gs://getting-termites-tweet/'+str(fname)
+    myurl='gs://processing-amazon-brand-analytics-search-terms-12345/'+str(fname)
     print('Uploaded file: {}'.format(event['name']))
     df=pd.read_csv(myurl)
     print('url is {}'.format(myurl))
     print('chunk shape is {}'.format(df.shape))
     date_until=datetime.today().strftime('%Y-%m-%d')
-    filename=('test_v4_{}.csv'.format(date_until))
+    filename=('{}-Top1MSearchTerms-AmazonUS.csv'.format(date_until))
     try:
         cols=df.columns.to_list()
         if "Daily" in cols[3]:
@@ -29,11 +29,9 @@ def change_and_save_csv_file(event,destination_bucket):
             df.rename(columns=df.iloc[0])
             df=df.drop(df.index[0])
             df['Report Date']=b
-            df=df[['Report Date','Department=[Amazon.com]', 'Search Term=[]',
-                    'Enter ASINs or Products=[]', 'Reporting Range=[Daily]',
-                    'Viewing=[3/20/21 - 3/20/21]', 'Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7',
-                    'Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12',
-                    'Unnamed: 13', 'Unnamed: 14']]
+            cols=df.columns.to_list()
+            cols = cols[-1:] + cols[:-1]
+            df=df[cols]
             df.columns=pd.Series(['Report Date', 'Department', 'Search Term', 'Search Frequency Rank',
                     '#1 Clicked ASIN', '#1 Product Title', '#1 Click Share',
                     '#1 Conversion Share', '#2 Clicked ASIN', '#2 Product Title',
@@ -45,7 +43,7 @@ def change_and_save_csv_file(event,destination_bucket):
             df.to_csv(f, index=False)
             f.seek(0)
             client=storage.Client()
-            bucket=client.get_bucket('my-image-data-bucket-2021')
+            bucket=client.get_bucket('final-amazon-brand-analytics-search-terms-12345')
             newblob=bucket.blob(filename)
             newblob.upload_from_string(f.read(), content_type='text/csv')
             print('uploaded storage')
--- rename_change_csv/check_duplication.py
+++ rename_change_csv/check_duplication.py
@@ -5,20 +5,20 @@ from io import StringIO
 from io import BytesIO
 import os
 
-os.environ["GCLOUD_PROJECT"] = "twittersheet-275317"
-project_id='twittersheet-275317'
-destination_bucket='getting-termites-tweet'
+os.environ["GCLOUD_PROJECT"] = "juva-brands"
+project_id='juva-brands'
+destination_bucket='dump-amazon-brand-analytics-search-terms-12345'
 
 def get_cols_list():
-  os.environ["GCLOUD_PROJECT"] = "my-bigquery-lab-286400"
+  os.environ["GCLOUD_PROJECT"] = "juva-brands"
   client=storage.Client()
-  bucket = client.bucket('getting-termites-tweet')
+  bucket = client.bucket('processing-amazon-brand-analytics-search-terms-12345')
   cols=[]
   for blob in bucket.list_blobs():
     print(blob.name)
     filename=blob.name
     try:   
-      myurl='gs://getting-termites-tweet/'+str(filename)
+      myurl='gs://processing-amazon-brand-analytics-search-terms-12345/'+str(filename)
       df=pd.read_csv(myurl, nrows=1)
       cols.append(df.columns[4])
       print(df.columns[4])
@@ -28,25 +28,29 @@ def get_cols_list():
 
 def check_and_trans(event,destination_bucket):
   client = storage.Client()
-  #please change the file's URI
+  bucket= client.get_bucket(event['bucket'])
+  blob = bucket.get_blob(event['name'])
   fname=event['name']
-  myurl='gs://staging.twittersheet-275317.appspot.com/'+str(fname)
+  myurl='gs://dump-amazon-brand-analytics-search-terms-12345/'+str(fname)
   print('Uploaded file: {}'.format(event['name']))
   df=pd.read_csv(myurl)
   print('url is {}'.format(myurl))
   print('chunk shape is {}'.format(df.shape))
   date_until=datetime.today().strftime('%Y-%m-%d')
-  filename=('test_v4_{}.csv'.format(date_until))
+  filename=('{}-Top1MSearchTerms-AmazonUS.csv'.format(date_until))
   cols=get_cols_list()
   if df.columns[4] not in cols:
     f = StringIO()
     df.to_csv(f, index=False)
     f.seek(0)
     client=storage.Client()
-    bucket=client.get_bucket('getting-termites-tweet')
-    newblob=bucket.blob("temp_file.csv")
+    bucket=client.get_bucket('processing-amazon-brand-analytics-search-terms-12345')
+    newblob=bucket.blob(filename)
     newblob.upload_from_string(f.read(), content_type='text/csv')
     print('uploaded storage')
+    blob.delete()
+    print("Blob {} deleted.".format(blob))
   else:
-    pass
+    blob.delete()
+    print("Blob {} deleted.".format(blob))
   return f'check the results in the logs'
